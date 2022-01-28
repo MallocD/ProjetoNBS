07532f2579004841776160feb17f3dc37d11440b
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-28 08:44:45 -0400

    backend-connect

---
+++ back-end/src/main/java/com/nbs/projectnbs/ProjectnbsApplication.java
@@ -0,0 +1,13 @@
+package com.nbs.projectnbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnbsApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/nbs/projectnbs/ProjectnbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbs.projectnbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

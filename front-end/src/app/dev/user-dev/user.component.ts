9e5fd14d8500e324078d43db03538d16e8ca3013
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-29 14:03:04 -0400

    Falta somente arrumar a tela de login

--- front-end/src/app/dev/user-dev/user.component.ts
+++ front-end/src/app/dev/user-dev/user.component.ts
@@ -1,8 +1,12 @@
 import { Component } from "@angular/core";
+import { User } from "./user";
 
 @Component({
     templateUrl: './user.component.html'
 })
 export class UserComponent{
+    user: User = new User();
+
+    
 
 }

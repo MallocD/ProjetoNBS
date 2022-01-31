aee9c3b8fa49e4d5d3a1371e65d5e8a7b915ef49
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-30 20:03:54 -0400

    System Login

--- front-end/src/app/dev/user-dev/user.component.ts
+++ front-end/src/app/dev/user-dev/user.component.ts
@@ -1,11 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { User } from "./user";
 
 @Component({
     templateUrl: './user.component.html'
 })
-export class UserComponent{
+export class UserComponent implements OnInit{
+    ngOnInit(): void {
+        this.user = {
+
+            name:"HelloDev",
+            password: '82313273'
+        }        
+        
+    }
+
+    constructor(private router: Router){
+
+    }
     user: User = new User();
+
+    login(){
+        if(this.user.name=="Malloc$Dev/"){
+            if(this.user.password=="devnbs$6731"){
+                this.router.navigate(['$/dev$'])
+            }
+        }else{
+            this.router.navigate(['/sdd'])
+        
+        }
+    }
+
+
 
     
 

64390b55d11a92f9e026fb90047f825bc085a833
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-02-03 07:55:44 -0400

    Exclude abstracts

--- back-end/src/main/java/com/nbs/projectnbs/repository/ClienteRepository.java
+++ back-end/src/main/java/com/nbs/projectnbs/repository/ClienteRepository.java
@@ -2,7 +2,6 @@ package com.nbs.projectnbs.repository;
 
 import com.nbs.projectnbs.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
